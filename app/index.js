31c3396b4d7c4ba43e0bfc7c3d1b08181b69b301
Helder <helder@lamoulade.com>
2016-03-10 13:11:10 +0000

    isntalled the react router, created route components and config rules

--- app/index.js
+++ app/index.js
@@ -6,59 +6,9 @@ var USER_DATA = {
 
 var React = require("react");
 var ReactDOM = require("react-dom");
-
-var Link = React.createClass({
-	changeURL: function () {
-		window.location.replace(this.props.href);
-	},
-	render: function () {
-		return (
-			<span style={{color: 'blue', cursor: 'pointer' }} onClick={this.changeURL}>
-				{this.props.children}
-			</span>
-		);
-	}
-});
-
-var ProfilePic = React.createClass({
-	render: function () {
-		return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
-	}
-});
-
-var ProfileLink = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<Link href={'https://github.com/' + this.props.username}>
-				{this.props.username}
-				</Link>
-			</div>
-		);
-	}
-});
-
-var ProfileName = React.createClass({
-	render: function () {
-		return (
-			<div>{this.props.name}</div>
-		);
-	}
-});
-
-var Avatar = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileName name={this.props.user.name} />
-				<ProfileLink username={this.props.user.username} />
-			</div>
-		);
-	}
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-	<Avatar user={USER_DATA} />,
+	routes,
 	document.getElementById('app')
 );
