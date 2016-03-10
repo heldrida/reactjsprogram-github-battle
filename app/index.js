4d535d401193b7dd17305f5a4bd9509598df6021
Helder <helder@lamoulade.com>
2016-03-10 12:47:55 +0000

    pure functions, props and nesting components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+	name: 'Helder Oliver',
+	username: 'heldrida',
+	image: 'https://avatars1.githubusercontent.com/u/236752?v=3&s=460'
+};
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{width: 100, height: 100}} />
+	}
+});
+
+var ProfileLink = React.createClass({
 	render: function () {
 		return (
-			<div>Hello world {this.props.name}!</div>
-		)
+			<div>
+				<a href={'https://github.com/' + this.props.username}>
+				{this.props.username}
+				</a>
+			</div>
+		);
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>{this.props.name}</div>
+		);
+	}
+});
+
+var Avatar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+		);
 	}
 });
 
 ReactDOM.render(
-	<HelloWorld name="Oliver" />,
+	<Avatar user={USER_DATA} />,
 	document.getElementById('app')
 );
