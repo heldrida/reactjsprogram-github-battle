097c7e53f30e584dcc5ca5394d724861076f08d2
Helder <helder@lamoulade.com>
2016-03-21 12:27:52 +0000

    created log for some life cycles

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -6,14 +6,31 @@ var ConfirmBattleContainer = React.createClass({
 		router: React.PropTypes.object.isRequired
 	},
 	getInitialState: function () {
+		console.log('getInitialState');
 		return {
 			isLoading: true,
 			playerInfo: []
 		}
 	},
+	componentWillMount: function () {
+		console.log("componentWillMount");
+	},
+	componentDidMount: function () {
+		console.log('componentDidMount');
+		var query = this.props.location.query;
+		// fetch info from github, then update the state
+	},
+	componentWillReceiveProps: function () {
+		console.log('componentWillReceiveProps');
+	},
+	componentWillUnmount: function () {
+		console.log('componentWillUnmount');
+	},
 	render: function () {
 		return (
-			<ConfirmBattle />
+			<ConfirmBattle
+				isLoading={this.state.isLoading}
+				playersInfo={this.state.playersInfo} />
 		);
 	}
 });
